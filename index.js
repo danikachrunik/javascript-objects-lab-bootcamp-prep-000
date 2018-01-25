0603808abf11228b3e5399c6287d1c863d3fe76e
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-01-25 01:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(recipes, {key: value});
+  return Object.assign({}, object, {key: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+  object[key] = value;
 }
 
 function deleteFromObjectByKey(object, key) {
